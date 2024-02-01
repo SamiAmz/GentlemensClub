e63ee13512d44330cf6cb15c39f04fe3257b0b8e
CristobalCrisostomo <158218250+CristobalCrisostomo@users.noreply.github.com>
2024-02-01 18:23:54 -0500

    header prototype

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,32 @@
 <template>
     <v-layout>
-        <v-app-bar :elevation="20">
-            <v-btn text to="/">Accueil</v-btn>
-            <v-btn text to="/">Cours</v-btn>
-            <v-btn text to="/">À propos</v-btn>
+
+        <v-app-bar absolute darkhide-on-scroll prominent scroll-target="#scrolling-techniques-4" color="grey"
+            :elevation="20">
+            <p id="texte">Gym de combat</p>
+            <v-row align="center" justify="center">
+                <router-link to="/">
+                    <v-btn outlined :ripple="false" text>Accueil</v-btn>
+                </router-link>
+                <router-link to="/">
+                    <v-btn outlined :ripple="false" text>Cours</v-btn>
+                </router-link>
+                <router-link to="/">
+                    <v-btn outlined :ripple="false" text>À propos</v-btn>
+                </router-link>
+            </v-row>
+            <p id="texte">login</p>
+            <router-link to="/" text>
+                <v-icon large color="black">
+                    mdi-login-variant
+                </v-icon>
+            </router-link>
         </v-app-bar>
     </v-layout>
 </template>
 
 <script>
+
 export default {
     name: 'AppHeader',
 }
@@ -16,11 +34,22 @@ export default {
 
 
 <style scoped>
+.v-btn {
+    margin: 10px 20px 10px 0;
+    padding: 10px 20px 20px 20px;
+    box-shadow: none;
+    background: none;
+    text-decoration: none;
+    font-size: 15px;
 
-.v-app-bar{
-    justify-content: center;
 }
-.v-btn{
-    justify-content: center;
+
+.v-app-bar {
+    background-color: rgb(28, 28, 28);
+}
+
+#texte {
+    color: black;
+    font-size: 20px;
 }
 </style>
