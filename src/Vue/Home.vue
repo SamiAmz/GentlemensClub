c52eb2b28083e3f0f8838e6adc8c2df0e75aa0f9
kein4ik <104735960+kein4ik@users.noreply.github.com>
2024-02-01 18:59:20 -0500

    save
    save

--- src/Vue/Home.vue
+++ src/Vue/Home.vue
@@ -1,5 +1,18 @@
+<template>
+  <v-container>
+    <v-toolbar-title class="mr-5"> Club de Champions </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn outlined class="mr-5" color="red"> Voir les abonnements. </v-btn>
+  </v-container>
+</template>
+
 <script>
 export default {
-    name: 'Home',
-}
+  name: "Home",
+  date() {
+    return {
+      athletes: [], //tableaux des athletes
+    };
+  },
+};
 </script>
