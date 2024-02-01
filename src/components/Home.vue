7ac864cd23a49e4cbfa22fd744569a20d1833e67
kein4ik <104735960+kein4ik@users.noreply.github.com>
2024-02-01 18:51:02 -0500

    homePage
    dsadasds

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
-   <v-img src = "@/assets/box.jpg"></v-img>
+  <v-container>
+    <v-app-bar app> </v-app-bar>
+  </v-container>
 </template>
 
 <script>
 export default {
-    name: 'Home',
-}
-
-
+  name: "Home",
+  date() {
+    return {
+      athletes: [], //tableaux des athletes
+    };
+  },
+};
 </script>
