8a92769e8636a64d741335cc25731ffe3fb67aa0
CristobalCrisostomo <158218250+CristobalCrisostomo@users.noreply.github.com>
2024-02-01 21:32:02 -0500

    main

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,31 +2,7 @@
     <v-layout>
 
         <v-app-bar id="semi-transparent-app-bar" color="transparent" :elevation="20">
-            <!-- Align items with space between them -->
-            <v-row align="center" justify="space-between" no-gutters>
-                <!-- "Gentlemen's club" aligned to the left -->
-                <img src = @/assets/logo.png> 
-                
-                <!-- Center content with no specific alignment -->
-                <div>
-                    <router-link to="/">
-                        <v-btn color="white" outlined :ripple="false" text>Accueil</v-btn>
-                    </router-link>
-                    <router-link to="/">
-                        <v-btn color="white" outlined :ripple="false" text>Cours</v-btn>
-                    </router-link>
-                    <router-link to="/">
-                        <v-btn color="white" outlined :ripple="false" text>À propos</v-btn>
-                    </router-link>
-                </div>
-                
-                <!-- "login" elements aligned to the right -->
-                <div>
-                    <router-link id="login" style="text-decoration: none; color: inherit" to="/">
-                        <p id="loginText">login</p>
-                        <v-icon large color="white">mdi-login-variant</v-icon>
 
-<<<<<<< Updated upstream
             <v-row align="center" justify="center">
                 <p id="title">Club de Champions</p>
                 <router-link to="/">
@@ -44,10 +20,6 @@
                 <router-link to="/">
                     <v-icon large color="white"> mdi-login-variant </v-icon>
                 </router-link>
-=======
-                    </router-link>
-                </div>
->>>>>>> Stashed changes
             </v-row>
         </v-app-bar>
     </v-layout>
@@ -78,14 +50,17 @@ export default {
 }
 
 #login {
-    display: flex; /* This will align the text and icon horizontally */
-    align-items: center; /* This will center the text and icon vertically */
+    display: flex;
+    /* This will align the text and icon horizontally */
+    align-items: center;
+    /* This will center the text and icon vertically */
     text-decoration: none;
     color: inherit;
 }
 
-#loginText {
-    margin-right: 8px; /* Adjust the space between the text and icon as needed */
+#loginTexte {
+    margin-right: 8px;
+    /* Adjust the space between the text and icon as needed */
     color: white;
     font-size: 20px;
 }
@@ -99,6 +74,4 @@ export default {
     text-decoration: none;
     font-size: 15px;
 }
-
-/* Remove duplicated CSS properties to clean up your code */
 </style>
