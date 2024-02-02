fafa3905e09e9b1a401b6e6f34fb44efdb59af28
CristobalCrisostomo <158218250+CristobalCrisostomo@users.noreply.github.com>
2024-02-01 19:07:11 -0500

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,26 +1,30 @@
 <template>
     <v-layout>
 
-        <v-app-bar absolute darkhide-on-scroll prominent scroll-target="#scrolling-techniques-4" color="black"
+        <v-app-bar absolute darkhide-on-scroll prominent scroll-target="#scrolling-techniques-4" color="grey"
             :elevation="20">
 
             <v-row align="center" justify="center">
                 <p id="title">Gym de combat</p>
                 <router-link to="/">
-                    <v-btn outlined :ripple="false" text>Accueil</v-btn>
+                    <v-btn color="black" outlined :ripple="false" text>Accueil</v-btn>
                 </router-link>
                 <router-link to="/">
-                    <v-btn outlined :ripple="false" text>Cours</v-btn>
+                    <v-btn color="black" outlined :ripple="false" text>Cours</v-btn>
                 </router-link>
                 <router-link to="/">
-                    <v-btn outlined :ripple="false" text>À propos</v-btn>
+                    <v-btn color="black" outlined :ripple="false" text>À propos</v-btn>
                 </router-link>
-                <p id="login">login</p>
+                <router-link id="login" style="text-decoration: none; color: inherit;" to="/">
+                    <p id="loginTexte">login</p>
+                </router-link>
                 <router-link to="/">
                     <v-icon large color="black">
                         mdi-login-variant
                     </v-icon>
                 </router-link>
+
+
             </v-row>
         </v-app-bar>
     </v-layout>
@@ -30,34 +34,35 @@
 import "@mdi/font/css/materialdesignicons.css";
 
 export default {
-  name: "AppHeader",
+    name: "AppHeader",
 };
 </script>
 
 <style scoped>
 .v-btn {
-  margin: 10px 20px 10px 0;
-  padding: 10px 20px 20px 20px;
-  box-shadow: none;
-  background: none;
-  text-decoration: none;
-  font-size: 15px;
+    margin: 10px 20px 10px 0;
+    padding: 10px 20px 20px 20px;
+    box-shadow: none;
+    background: none;
+    text-decoration: none;
+    font-size: 15px;
 }
 
 .v-app-bar {
-  background-color: rgb(28, 28, 28);
+    background-color: rgb(28, 28, 28);
 }
 
 #login {
+    margin-left: 5dvw;
+}
+#loginTexte{
     color: black;
     font-size: 20px;
-    margin-left: 5dvw;
-
 }
 
 #title {
     color: black;
     font-size: 20px;
-margin-right: 5dvw;
+    margin-right: 5dvw;
 }
 </style>
