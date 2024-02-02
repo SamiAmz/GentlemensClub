cf0c4646c1c308d8dd2876f3396287a31ab9eb90
CristobalCrisostomo <158218250+CristobalCrisostomo@users.noreply.github.com>
2024-02-01 22:48:09 -0500

    image

--- src/Vue/Home.vue
+++ src/Vue/Home.vue
@@ -6,12 +6,7 @@
     </video>
     <v-container>
       <v-carousel cycle interval="3000" hide-delimiters>
-        <v-carousel-item
-          v-for="(item, i) in items"
-          :key="i"
-          :src="item"
-          cover
-        ></v-carousel-item>
+        <v-carousel-item v-for="(item, i) in items" :key="i" :src="item" cover></v-carousel-item>
       </v-carousel>
     </v-container>
     <v-container>
@@ -21,8 +16,7 @@
             <v-card class="mx-auto my-8" elevation="16">
               <v-card-title>
                 🥊 Découvrez l'essence de la puissance physique et mentale !
-                🥋</v-card-title
-              >
+                🥋</v-card-title>
               <v-card-text>
                 <p>
                   Au [Nom du Gym de Combat], nous vous proposons bien plus qu'un
@@ -36,8 +30,7 @@
             <v-card class="mx-auto my-8" elevation="16">
               <v-card-title>
                 🔥 Devenez membre aujourd'hui et transformez votre vie !
-                🔥</v-card-title
-              >
+                🔥</v-card-title>
               <v-card-text>
                 <p>
                   Rejoignez-nous dès maintenant pour profiter de tous les
@@ -45,15 +38,12 @@
                   voyage vers une meilleure version de vous-même !
                 </p>
               </v-card-text>
-              <v-card-actions
-                >[Inscrivez-vous maintenant] [Numéro de Contact] [Adresse du
-                Gym]</v-card-actions
-              >
+              <v-card-actions>[Inscrivez-vous maintenant] [Numéro de Contact] [Adresse du
+                Gym]</v-card-actions>
             </v-card>
             <v-card class="mx-auto my-8" elevation="16">
               <v-card-title>
-                🥋 Cours Quotidiens pour Adultes ! 💪</v-card-title
-              >
+                🥋 Cours Quotidiens pour Adultes ! 💪</v-card-title>
               <v-card-text>
                 <p>
                   Plongez-vous dans l'intensité du combat chaque jour au [Nom du
@@ -70,9 +60,7 @@
           <v-col>
             <v-card class="mx-auto my-8" max-width="600" elevation="16">
               <v-card-title>Ce que nous offrons :</v-card-title>
-              <v-card-subtitle
-                >Cours de Combat de Classe Mondiale :</v-card-subtitle
-              >
+              <v-card-subtitle>Cours de Combat de Classe Mondiale :</v-card-subtitle>
               <v-card-text>
                 De la boxe à la lutte, en passant par les arts martiaux mixtes
                 (MMA), nos instructeurs expérimentés vous guideront à travers
@@ -114,8 +102,8 @@
 
 <script>
 import item1 from "@/assets/box3.png";
-import item2 from "@/assets/box.jpg";
-import item3 from "@/assets/box2.jpg";
+import item2 from "@/assets/box3.png";
+import item3 from "@/assets/box3.png";
 export default {
   name: "Home",
   data() {
