9c3be46d55cfb9a4bbb867fe619711943e27f5f1
CristobalCrisostomo <158218250+CristobalCrisostomo@users.noreply.github.com>
2024-02-01 22:44:15 -0500

    home

--- src/Vue/Home.vue
+++ src/Vue/Home.vue
@@ -91,8 +91,8 @@
 
 <script>
 import item1 from "@/assets/box3.png";
-import item2 from "@/assets/box2.jpg";
-import item3 from "@/assets/box.jpg";
+import item2 from "@/assets/box3.png";
+import item3 from "@/assets/box3.png";
 export default {
   name: "Home",
   data() {
