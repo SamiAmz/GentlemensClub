8c0e45230fdaebbf18ed9ba1f6caacededa7a96a
CristobalCrisostomo <158218250+CristobalCrisostomo@users.noreply.github.com>
2024-02-02 16:10:43 -0500

    better cards

--- src/Vue/Home.vue
+++ src/Vue/Home.vue
@@ -9,107 +9,28 @@
         <v-carousel-item v-for="(item, i) in items" :key="i" :src="item" cover></v-carousel-item>
       </v-carousel>
     </v-container>
-    <v-container>
-      <v-container>
-        <v-row>
-          <v-col>
-            <v-card class="mx-auto my-8" elevation="16">
-              <v-card-title>
-                🥊 Découvrez l'essence de la puissance physique et mentale !
-                🥋</v-card-title>
-              <v-card-text>
-                <p>
-                  Au [Nom du Gym de Combat], nous vous proposons bien plus qu'un
-                  simple entraînement. Immergez-vous dans l'univers exceptionnel
-                  du combat, où la discipline, la force et la technique
-                  fusionnent pour créer une expérience véritablement
-                  transformante.
-                </p>
-              </v-card-text>
-            </v-card>
-            <v-card class="mx-auto my-8" elevation="16">
-              <v-card-title>
-                🔥 Devenez membre aujourd'hui et transformez votre vie !
-                🔥</v-card-title>
-              <v-card-text>
-                <p>
-                  Rejoignez-nous dès maintenant pour profiter de tous les
-                  avantages exclusifs réservés à nos membres. Démarrez votre
-                  voyage vers une meilleure version de vous-même !
-                </p>
-              </v-card-text>
-              <v-card-actions>[Inscrivez-vous maintenant] [Numéro de Contact] [Adresse du
-                Gym]</v-card-actions>
-            </v-card>
-            <v-card class="mx-auto my-8" elevation="16">
-              <v-card-title>
-                🥋 Cours Quotidiens pour Adultes ! 💪</v-card-title>
-              <v-card-text>
-                <p>
-                  Plongez-vous dans l'intensité du combat chaque jour au [Nom du
-                  Gym de Combat] ! Rejoignez notre programme exclusif réservé
-                  aux adultes pour perfectionner vos compétences physiques et
-                  mentales. Transformez votre routine avec des sessions
-                  quotidiennes passionnantes. Prêt à relever le défi ? 💥
-                  [Inscrivez-vous maintenant] [Numéro de Contact] [Adresse du
-                  Gym]
-                </p>
-              </v-card-text>
-            </v-card>
-          </v-col>
-          <v-col>
-            <v-row>
-              <v-col>
-                <v-card class="mx-auto my-8" max-width="600" elevation="16">
-                  <v-card-title>🔒 art martiau 🥋</v-card-title>
-                  <v-card-subtitle>subtitle :</v-card-subtitle>
-                  <v-card-text>
-                    text
-                    <br>
-                    text
-                    <br>
-                    text
-                    <br>
-                    text
-                  </v-card-text>
-                </v-card>
-              </v-col>
-              <v-col>
-                <v-card class="mx-auto my-8" max-width="600" elevation="16">
-                  <v-card-title>🔒 art martiau 🥋</v-card-title>
-                  <v-card-subtitle>subtitle :</v-card-subtitle>
-                  <v-card-text>
-                    text
-                    <br>
-                    text
-                    <br>
-                    text
-                    <br>
-                    text
-                  </v-card-text>
-                </v-card>
-              </v-col>
-            </v-row>
-            <v-card class="mx-auto my-8" max-width="600" elevation="16">
-              <v-card-title>🔒 art martiau 🥋</v-card-title>
-              <v-card-subtitle>subtitle :</v-card-subtitle>
-              <v-card-text>
-                text
-                <br>
-                text
-                <br>
-                text
-                <br>
-                text
-              </v-card-text>
-            </v-card>
+    <v-container class="d-flex flex-row justify-space-between">
+      <v-card class="mx-auto my-8" elevation="16">
+        <v-card-title>
+          <p class="font-weight-light">Art Martial</p>
 
+        </v-card-title>
+        <v-img src="@/assets/wrestling-logo.png"></v-img>
+      </v-card>
+      <v-card class="mx-auto my-8" elevation="16">
+        <v-card-title>
+          <p class="font-weight-light">Art Martial</p>
 
+        </v-card-title>
+        <v-img src="@/assets/wrestling-logo.png"></v-img>
+      </v-card>
+      <v-card class="mx-auto my-8" elevation="16">
+        <v-card-title>
+          <p class="font-weight-light">Art Martial</p>
 
-          </v-col>
-
-        </v-row>
-      </v-container>
+        </v-card-title>
+        <v-img src="@/assets/wrestling-logo.png"></v-img>
+      </v-card>
     </v-container>
   </div>
 </template>
@@ -136,19 +57,30 @@ video {
 }
 
 .v-card {
-  background-color: gray;
+  background: repeating-linear-gradient(45deg, rgb(255, 215, 0), rgb(254, 241, 150), rgb(250, 217, 30), rgb(255, 217, 0));
+  width: 23vw;
+  height: 30vw;
+  border: 2px solid #FFD700;
+  border-radius: 35px;
+  transition: transform 0.3s ease-in-out;
+}
+
+.v-card:hover {
+  transform: scale(1.1);
 }
 
 .v-card-title {
-  color: rgba(87, 0, 0, 0.603);
+  text-align: center;
+  font-size: 3.5em;
+  padding-top: 90px;
+}
+
+.v-img {
+  border-radius: 35px;
 }
 
 .v-card-subtitle {
   color: rgb(255, 255, 255);
-}
-
-.v-card-text {
-  margin: 0;
 }
 
 #page-container {
