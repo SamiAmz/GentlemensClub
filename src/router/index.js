43c3b21cd7cd4e318238bc666e18767543254f46
Baguette74 <rienmaster@gmail.com>
2024-02-06 14:22:31 -0500

    added calendar demo

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../vue/Home.vue";
-import Contact from "../components/Contact.vue"
+import Contact from "../components/Contact.vue";
+import Horaire from "../components/Horaire.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,12 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: Home,
+    },
+
+    {
+      path: "/horaire",
+      name: "horaire",
+      component: Horaire
     },
 
     {
