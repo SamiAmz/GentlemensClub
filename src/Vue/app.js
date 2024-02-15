fb07d80f3e2c3878986b4e46fb86a8850a4c3322
CristobalCrisostomo <158218250+CristobalCrisostomo@users.noreply.github.com>
2024-02-15 10:14:20 -0500

    CAROUSSEL

---
+++ src/Vue/app.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Get DOM elements
+  let nextDom = document.getElementById("next");
+  let prevDom = document.getElementById("prev");
+  let carouselDom = document.querySelector(".carousel");
+  let SliderDom = carouselDom.querySelector(".carousel .list");
+  let thumbnailBorderDom = document.querySelector(".carousel .thumbnail");
+  let timeDom = document.querySelector(".carousel .time");
+
+  // Initial setup
+  let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
+  thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+  let timeRunning = 600;
+  let timeAutoNext = 7000;
+  let runTimeOut;
+  let runNextAuto = setTimeout(() => {
+    next.click();
+  }, timeAutoNext);
+
+  // Click event handlers
+  nextDom.onclick = function () {
+    showSlider("next");
+  };
+
+  prevDom.onclick = function () {
+    showSlider("prev");
+  };
+
+  // Function to show slider
+  function showSlider(type) {
+    let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
+    let thumbnailItemsDom = document.querySelectorAll(
+      ".carousel .thumbnail .item"
+    );
+
+    if (type === "next") {
+      SliderDom.appendChild(SliderItemsDom[0]);
+      thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+      carouselDom.classList.add("next");
+    } else {
+      SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+      thumbnailBorderDom.prepend(
+        thumbnailItemsDom[thumbnailItemsDom.length - 1]
+      );
+      carouselDom.classList.add("prev");
+    }
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+      carouselDom.classList.remove("next");
+      carouselDom.classList.remove("prev");
+    }, timeRunning);
+
+    clearTimeout(runNextAuto);
+    runNextAuto = setTimeout(() => {
+      next.click();
+    }, timeAutoNext);
+  }
+});
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,35 +1,73 @@
 <template>
-  <v-layout>
+  <v-layout id="container">
     <v-app-bar id="semi-transparent-app-bar" :elevation="20">
       <v-row align="center" justify="space-between">
         <v-img max-width="90" max-height="100" src="@/assets/logo.png"></v-img>
 
         <div>
           <router-link to="/" exact active-class="button-active">
-            <v-btn class="button-hover" color="white" outlined :ripple="false" text>
+            <v-btn
+              class="button-hover"
+              color="white"
+              outlined
+              :ripple="false"
+              text
+            >
               Accueil
             </v-btn>
           </router-link>
           <router-link to="/horaire" exact active-class="button-active">
-            <v-btn class="button-hover" color="white" outlined :ripple="false" text>
+            <v-btn
+              class="button-hover"
+              color="white"
+              outlined
+              :ripple="false"
+              text
+            >
               Horaire
             </v-btn>
           </router-link>
           <router-link to="/contact" exact active-class="button-active">
-            <v-btn class="button-hover" color="white" outlined :ripple="false" text>
+            <v-btn
+              class="button-hover"
+              color="white"
+              outlined
+              :ripple="false"
+              text
+            >
               À propos
             </v-btn>
           </router-link>
         </div>
 
         <div>
-          <router-link v-if="isLoggedIn" to="/profile" id="profile" style="text-decoration: none; color: inherit; display: flex; align-items: center;">
+          <router-link
+            v-if="isLoggedIn"
+            to="/profile"
+            id="profile"
+            style="
+              text-decoration: none;
+              color: inherit;
+              display: flex;
+              align-items: center;
+            "
+          >
             <p id="profileText">Profil</p>
             <v-icon large color="white">mdi-account</v-icon>
           </router-link>
 
           <!-- Connexion Link (Shown when Logged Out) -->
-          <router-link v-else id="login" to="/login" style="text-decoration: none; color: inherit; display: flex; align-items: center;">
+          <router-link
+            v-else
+            id="login"
+            to="/login"
+            style="
+              text-decoration: none;
+              color: inherit;
+              display: flex;
+              align-items: center;
+            "
+          >
             <p id="loginTexte">Connexion</p>
             <v-icon large color="white">mdi-login-variant</v-icon>
           </router-link>
@@ -38,7 +76,6 @@
     </v-app-bar>
   </v-layout>
 </template>
-
 
 <style scoped>
 :deep(.button-active .v-btn__content) {
@@ -107,7 +144,7 @@ export default {
   name: "AppHeader",
   props: {
     isLoggedIn: Boolean,
-  }
+  },
 };
 
 document.addEventListener("DOMContentLoaded", function () {
