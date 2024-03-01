c132276aab38a16f452e7b2fc08468bd3d5bc57b
SamiAmz <129106024+SamiAmz@users.noreply.github.com>
2024-02-29 21:48:17 -0500

    Update create-checkout-sessions.js

--- functions/create-checkout-sessions.js
+++ functions/create-checkout-sessions.js
@@ -1,13 +1,19 @@
-// Require the Stripe library
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-exports.handler = async (event, context) => {
+exports.handler = async function(event) {
+  console.log("Received event:", event); // Log the incoming event
+
   if (event.httpMethod !== 'POST') {
+    console.log("Method Not Allowed");
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
 
   try {
+    console.log("Parsing body:", event.body);
     const { priceId } = JSON.parse(event.body);
+    console.log("Received priceId:", priceId); // Log the received priceId
+
+    console.log("Creating Stripe Checkout Session");
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ['card'],
       line_items: [{
@@ -16,14 +22,16 @@ exports.handler = async (event, context) => {
       }],
       mode: 'subscription',
       success_url: `${process.env.SITE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: 'https://stripe--dancing-centaur-316b8d.netlify.app',
+      cancel_url: `${process.env.SITE_URL}`,
     });
 
+    console.log("Session created:", session.id); // Log the session ID
     return {
       statusCode: 200,
       body: JSON.stringify({ sessionId: session.id }),
     };
   } catch (error) {
+    console.error("Error creating session:", error); // Log any errors
     return {
       statusCode: 500,
       body: JSON.stringify({ error: error.message }),
