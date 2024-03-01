fe4ff5d6cd739488b8418f308b520a29fbec126d
SamiAmz <129106024+SamiAmz@users.noreply.github.com>
2024-02-29 21:52:05 -0500

    Update create-checkout-sessions.js

--- functions/create-checkout-sessions.js
+++ functions/create-checkout-sessions.js
@@ -1,40 +1,35 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
 exports.handler = async function(event) {
-  console.log("Received event:", event); // Log the incoming event
+  const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
   if (event.httpMethod !== 'POST') {
-    console.log("Method Not Allowed");
-    return { statusCode: 405, body: 'Method Not Allowed' };
+      return { statusCode: 405, body: 'Method Not Allowed' };
   }
 
   try {
-    console.log("Parsing body:", event.body);
-    const { priceId } = JSON.parse(event.body);
-    console.log("Received priceId:", priceId); // Log the received priceId
+      // Extract the base URL from the Referer header
+      const refererUrl = new URL(event.headers.referer);
+      const baseUrl = `${refererUrl.protocol}//${refererUrl.host}`;
 
-    console.log("Creating Stripe Checkout Session");
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: [{
-        price: priceId,
-        quantity: 1,
-      }],
-      mode: 'subscription',
-      success_url: `${process.env.SITE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${process.env.SITE_URL}`,
-    });
+      const { priceId } = JSON.parse(event.body);
+      const session = await stripe.checkout.sessions.create({
+          payment_method_types: ['card'],
+          line_items: [{
+              price: priceId,
+              quantity: 1,
+          }],
+          mode: 'subscription',
+          success_url: `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
+          cancel_url: `${baseUrl}/cancel`,
+      });
 
-    console.log("Session created:", session.id); // Log the session ID
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ sessionId: session.id }),
-    };
+      return {
+          statusCode: 200,
+          body: JSON.stringify({ sessionId: session.id }),
+      };
   } catch (error) {
-    console.error("Error creating session:", error); // Log any errors
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
+      return {
+          statusCode: 500,
+          body: JSON.stringify({ error: error.message }),
+      };
   }
 };
