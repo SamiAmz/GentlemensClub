3b7acef6a05325aab52dc29357bd7032e361e874
SamiAmz <129106024+SamiAmz@users.noreply.github.com>
2024-03-04 13:07:28 -0500

    Update payment-details.js

--- functions/payment-details.js
+++ functions/payment-details.js
@@ -1,4 +1,3 @@
-// Require the Stripe library
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async function(event) {
@@ -12,17 +11,36 @@ exports.handler = async function(event) {
     const { sessionId } = JSON.parse(event.body);
     console.log('Received sessionId:', sessionId); // Log the received sessionId for debugging
 
-    // Retrieve the session details from Stripe
-    const session = await stripe.checkout.sessions.retrieve(sessionId);
-    console.log('Session details retrieved:', session); // Optional: Log the session details for debugging
+    // Retrieve the session details from Stripe, expanding the subscription
+    const session = await stripe.checkout.sessions.retrieve(sessionId, {
+      expand: ['subscription'],
+    });
+
+    // Check if the session has a subscription object and get the current_period_end
+    let expirationDate;
+    if (session.subscription) {
+      expirationDate = session.subscription.current_period_end;
+    } else {
+      // Handle cases where there is no subscription object linked to the session
+      console.error('No subscription found for this session');
+      expirationDate = null; // or set a default value, or handle as needed
+    }
+
+    // Extract other necessary details from the session or subscription
+    const amount = session.amount_total; // The total amount of the session
+    const status = session.payment_status; // The payment status of the session
+    // ... add logic to determine the 'type' or other properties if needed
 
     // Return the necessary session details to the client
     return {
       statusCode: 200,
       body: JSON.stringify({
         sessionId: session.id,
-        // Include any other session details you need
-        // You might want to extract specific details from the session object to return to the client
+        expirationDate: expirationDate, // Unix timestamp of the period end
+        amount: amount,
+        status: status,
+        type: "Boxe", // Assuming this is static or derived from somewhere else in your app
+        // ... include any other fields you need to return
       }),
     };
   } catch (error) {
