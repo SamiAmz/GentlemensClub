ffed174122471ae6d2b5e172a71561e1c9c7a318
SamiAmz <129106024+SamiAmz@users.noreply.github.com>
2024-03-05 15:35:20 -0500

    Update subscribe.js

--- src/components/Sports/subscribe.js
+++ src/components/Sports/subscribe.js
@@ -16,9 +16,10 @@ export async function subscribeToCourse(priceId, courseType) {
     const response = await fetch("/.netlify/functions/create-checkout-sessions", {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ priceId: priceId, courseType: courseType }), // Pass courseType here
+      body: JSON.stringify({ priceId: priceId, courseType: "boxe" }), // Pass courseType here
     });
 
+    
     const session = await response.json();
     if (response.ok && session.sessionId) {
       const { error } = await stripe.redirectToCheckout({
